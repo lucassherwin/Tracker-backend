9fd2cbd09850141886d516f84fb5b6a751cb74ca
lucassherwin <lksherwin@gmail.com>
2021-03-15 16:58:36 -0400

    able to create a user in mongoDB

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,8 +5,13 @@ const User = mongoose.model('User');
 
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
-  console.log(req.body); // gets the data sent in the post request
+router.post('/signup', async (req, res) => {
+  console.log(req.body); // gets the data sent in the post request {email password}
+  
+  const { email, password } = req.body; // get the email and password
+  const user = new User({ email, password }); // create a new user
+  await user.save(); // save user in mongoDB
+
   res.send('You made a post request');
 });
 
